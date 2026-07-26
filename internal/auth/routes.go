cabab9c378c326dd872a6031f09758220683b312
LuisTinocoC <140081629+LuisTinocoC@users.noreply.github.com>
2026-07-26 18:37:15 -0500

    update the urls

--- internal/auth/routes.go
+++ internal/auth/routes.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// RegisterRoutes @Router /arkivy/api/v1.0
 func RegisterRoutes(rg *gin.RouterGroup, h *Handler) {
 	auth := rg.Group("/auth")
 	auth.POST("/login", h.Login)
